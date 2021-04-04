b208a70fae1c3e2361642a85277fe48d1ba0e447
ronig <ronigu@post.bgu.ac.il>
2021-04-04 20:03:01 +0300

    Version 4 - Empty Maze

---
+++ src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -0,0 +1,10 @@
+package algorithms.mazeGenerators;
+
+
+public class EmptyMazeGenerator extends AMazeGenerator {
+
+    @Override
+    public Maze generate(int rows, int columns) {
+        return new Maze(rows, columns);
+    }
+}
