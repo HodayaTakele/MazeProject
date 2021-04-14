c6d150819540aad2f43621b50f81bbace493f089
HodayaTakele <hodayat@post.bgu.ac.il>
2021-04-14 15:17:25 +0300

    Version 14 Added and changed methods in the following classes-  BestFirstSearch - finished

--- src/algorithms/search/BestFirstSearch.java
+++ src/algorithms/search/BestFirstSearch.java
@@ -1,16 +1,17 @@
 package algorithms.search;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class BestFirstSearch extends ASearchingAlgorithm{
+    LinkedList<AState> closeList;
 
     public BestFirstSearch(){
         this.visitedNodesCount = 0;
-        this.openList = new PriorityQueue<>(2, new StateCostComparator());
-    }
-    private AState search( AState currState){
-
-        return null;
+        this.openList = new PriorityQueue<>(1, new StateCostComparator());
+        this.closeList = new LinkedList<>();
     }
 
     @Override
@@ -20,10 +21,39 @@ public class BestFirstSearch extends ASearchingAlgorithm{
 
     @Override
     public Solution solve(ISearchable searchable) {
-        //AState startState = searchable.getStartState();
-        AState[] stateSuccessors = searchable.getAllSuccessors(searchable.getStartState());
+        if(searchable == null){
+            //throw exception
+        }
 
-
-        return null;
+        Solution sol = null;
+        this.openList.add(searchable.getStartState());
+        AState finalState = searchable.getFinalState();
+        // Run until the opnList is Empty
+        while (!(this.openList.isEmpty())){
+            AState currState = this.openList.poll();
+            AState[] stateSuccessors = searchable.getAllSuccessors(currState);
+            // Run over currState successors and add them to openList while searching
+            for (AState successor: stateSuccessors){
+                if( (successor != null) && (successor.equals(finalState)) && (successor.getCost() < finalState.getCost() || finalState.getCost() == 0) ){
+                    finalState = successor;
+                }
+                else if(successor != null && !(this.closeList.contains(currState))){
+                    if(!this.openList.contains(successor)){
+                        this.openList.add(successor);
+                    }
+                    else if(this.openList.contains(successor)){
+                        //is there a easier way to find the successor without .stream().filter() which return a "set"
+                        int olderCost = this.openList.stream().filter(o->o.equals(successor)).mapToInt(o->o.getCost()).sum();
+                        if ( successor.getCost() <= olderCost ){
+                            this.openList.remove(successor);
+                            this.openList.add(successor);
+                        }
+                    }
+                }
+            }
+            this.closeList.add(currState);
+        }
+        this.visitedNodesCount = this.closeList.size();
+        return new Solution(finalState);
     }
 }
