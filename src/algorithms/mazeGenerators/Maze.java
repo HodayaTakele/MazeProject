e709ca78a468540ed7ffcdb9b980824f23755983
HodayaTakele <hodayat@post.bgu.ac.il>
2021-05-03 21:23:01 +0300

    Version 24 Maze.toByteArray() method added

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -1,5 +1,7 @@
 package algorithms.mazeGenerators;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Maze {
@@ -33,6 +35,53 @@ public class Maze {
             this.data[i] = maze.data[i].clone();
         }
     }
+
+    /**
+     * mazeByte[0] - represent the number of cells we use for maze information
+     * if mazeByte[0] = 0 : we use the first 5 bytes for information.
+     *          mazeByte[1] = rows; mazeByte[2] = columns; mazeByte[3] = goal.getRowIndex(); mazeByte[4] = goal.getColumnIndex(); mazeByte[5: ] = maze data
+     * if mazeByte[0] = 1 : we use the first 17 bytes for information.
+     *          mazeByte[1:4] = rows; mazeByte[5:8] = columns; mazeByte[9:12] = goal.getRowIndex(); mazeByte[13:16] = goal.getColumnIndex(); mazeByte[17: ] = maze data
+     * @return byte[] = [rows, columns, goal, maze data] - with the above decomposition
+     */
+    public byte[] toByteArray(){
+        byte[] mazeByte;
+        boolean infoByte;
+        int b;
+        if (rows<255 && columns<255 && goal.getRowIndex()<255 && goal.getColumnIndex()<255 ){
+            b = 5;
+            mazeByte = new byte[5 + (rows+1)*(columns+1)];
+            mazeByte[0] = 0;
+            mazeByte[1] = (byte) rows;
+            mazeByte[2] = (byte)columns;
+            mazeByte[3] = (byte)goal.getRowIndex();
+            mazeByte[4] = (byte)goal.getColumnIndex();
+        }
+        else{
+            b = 17;
+            mazeByte = new byte[17 + (rows+1)*(columns+1)];
+            mazeByte[0] = 1;
+            ArrayList<Integer> info = new ArrayList<>(Arrays.asList(rows,columns,goal.getRowIndex(),goal.getColumnIndex()));
+            ArrayList<byte[]> mazeInfo = new ArrayList<>();
+            for (Integer in: info) {
+                mazeInfo.add(ByteBuffer.allocate(4).putInt(in).array());
+            }
+            for (int i = 0; i < 4; i++) {
+                byte[] temp = mazeInfo.get(i);
+                for (int j = 0; j < 4; j++) {
+                    mazeByte[(i*4)+j+1] = temp[j];
+                }
+            }
+        }
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= columns; j++) {
+                mazeByte[b+(i*rows)+j] = (byte) data[i][j];
+            }
+        }
+        return mazeByte;
+    }
+
+
 
     public void setStart(int row, int column) throws MazeException {
         if ( row < 0 || row > this.rows ) throw new MazeException("row", this.rows);
