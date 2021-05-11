6781f76e7cd13e68d542492a8ceb922f509026f7
HodayaTakele <hodayat@post.bgu.ac.il>
2021-05-11 13:47:17 +0300

    Version 34 - ISearchable - added description

--- src/main/java/algorithms/search/ISearchable.java
+++ src/main/java/algorithms/search/ISearchable.java
@@ -5,7 +5,7 @@ public interface ISearchable {
     AState getGoalState();
 
     /**Return the State's successors States
-     * @param currState
+     * @param currState the state that we wish to find it's successors
      * @return an array of all the States that the current State can move to.
      */
     AState[] getAllSuccessors(AState currState);
